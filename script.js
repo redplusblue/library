7f76d17bc78ce54963482f726bd8fbe31fd4b8d0
sk <samirkabra1161@gmail.com>
2022-08-30 17:01:53 -0400

    Added add button, form
    Finished project

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 let library = [];
 let container = document.getElementById('container');
+let addBook = document.getElementById('add-btn')
+let addNewBook = document.getElementById('add-book')
+let formContainer = document.getElementById('form-container');
+
+addBook.addEventListener('click', () => {
+    if(formContainer.style.display == 'none') {
+        formContainer.style.display = 'flex';
+    } else {
+        formContainer.style.display = 'none';
+    }
+})
+
+addNewBook.addEventListener('click', () => {
+    let title = document.getElementById('title-input').value;
+    let author = document.getElementById('author-input').value;
+    let pages = Number(document.getElementById('page-input').value);
+    let read = document.getElementById('read-input').checked;
+    addBooks(title, author, pages, read);
+    formContainer.style.display = 'none';
+})
 
 class book {
     title = "";
@@ -18,14 +38,14 @@ function getBookFromLibrary(index) {
 
 function showBooks() {
     library.forEach(function (book) {
-       newBook = document.createElement('div');
+       let newBook = document.createElement('div');
+       let newTitle = document.createElement('span'); 
+       let newAuthor = document.createElement('span');
+       let newPages = document.createElement('span');
+       let newReadButton = document.createElement('button');
+       let newDeleteButton = document.createElement('button');
        newBook.id = 'book';
-       newTitle = document.createElement('span'); 
-       newAuthor = document.createElement('span');
-       newPages = document.createElement('span');
-       newReadButton = document.createElement('button');
        newReadButton.id = 'read-button';
-       newDeleteButton = document.createElement('button');
        newDeleteButton.id = 'delete';
 
        newTitle.innerText = book.title;
@@ -49,27 +69,45 @@ function showBooks() {
        newBook.appendChild(newReadButton);
        newBook.appendChild(newDeleteButton);
        container.appendChild(newBook);
+       refreshNodeLists();
     })
 }
 
-harryPotter = new book();
-harryPotter.title = "Harry Potter and something that happened";
-harryPotter.author = "J.K. Rowling";
-harryPotter.pages = 400;
-harryPotter.read = true;
-addBookToLibrary(harryPotter);
-showBooks()
+function addBooks(title, author, pages, read) {
+    if(title.length != 0 && author.length != 0 && typeof(pages) == 'number' && typeof(read) == 'boolean') {
+        let newBook = new book();
+        newBook.title = title;
+        newBook.author = author;
+        newBook.pages = pages;
+        newBook.read = read;
+        addBookToLibrary(newBook)
+        showBooks()
+    } else {
+        alert("Invalid input!")
+    }
+}
 
-let readButton = document.querySelectorAll("#read-button");
-readButton.forEach(function (button) {
-    button.addEventListener("click", function () {
-        if(button.innerText === 'Read') {
-            button.innerText = 'Unread';
-            button.className = 'unread';
-            // Code to make changes in the database as well
-        } else {
-            button.innerText = 'Read';
-            button.className = 'read';
-        }
-    }); 
-});
+function refreshNodeLists() {
+    let readButton = document.querySelectorAll("#read-button");
+    readButton.forEach(function (button) {
+        button.addEventListener("click", function () {
+            if(button.innerText === 'Read') {
+                button.innerText = 'Unread';
+                button.className = 'unread';
+                // Code to make changes in the database as well
+            } else {
+                button.innerText = 'Read';
+                button.className = 'read';
+            }
+        }); 
+    });
+
+    let deleteButton = document.querySelectorAll('#delete');
+    deleteButton.forEach((button) => {
+        button.addEventListener('click', function() {
+            isConfirmed = confirm(`Are you sure you want to delete the book - "${this.parentNode.firstChild.innerText}"`)
+            if(isConfirmed){
+            container.removeChild(this.parentNode)}
+        })
+    })
+}
